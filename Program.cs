5f0a2de9b4b2a7520eae62ac11e00fc36ac0169d
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 10:24:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Abstact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Corsa corsa = new Corsa();
+            Console.WriteLine(corsa.weels);
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Abstact
+{
+  abstract class Vehicle
+    {
+        public int weels = 4;
+    }
+}
